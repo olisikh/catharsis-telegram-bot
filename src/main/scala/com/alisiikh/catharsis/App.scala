31c9770b925fabb333eb10e38eacb323f3d50f22
Oleksii Lisikh <alisiikh@gmail.com>
2021-06-09 11:16:04 +0200

    Updated logging library

--- src/main/scala/com/alisiikh/catharsis/App.scala
+++ src/main/scala/com/alisiikh/catharsis/App.scala
@@ -4,8 +4,8 @@ import cats.effect._
 import cats.implicits._
 import com.alisiikh.catharsis.bot.CatharsisBotProcess
 import fs2.Stream
-import io.chrisdavenport.log4cats.Logger
-import io.chrisdavenport.log4cats.slf4j.Slf4jLogger
+import org.typelevel.log4cats.Logger
+import org.typelevel.log4cats.slf4j.Slf4jLogger
 
 import scala.language.higherKinds
 
