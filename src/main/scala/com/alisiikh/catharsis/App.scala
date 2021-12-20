de0a333d003286440fb3a08e1e1a780717336379
Oleksii Lisikh <alisiikh@gmail.com>
2021-12-20 22:48:05 +0100

    Left a TODO and prettified the code

--- src/main/scala/com/alisiikh/catharsis/App.scala
+++ src/main/scala/com/alisiikh/catharsis/App.scala
@@ -2,15 +2,11 @@ package com.alisiikh.catharsis
 
 import cats.effect._
 import cats.implicits._
-import com.alisiikh.catharsis.bot.{ BotToken, TelegramBot }
-import com.alisiikh.catharsis.bot.api.TelegramBotApi
-import com.alisiikh.catharsis.giphy.{ GiphyClient, GiphyToken }
+import com.alisiikh.catharsis.bot.{ BotProcess, BotToken }
+import com.alisiikh.catharsis.giphy.GiphyToken
 import fs2.Stream
-import org.http4s.blaze.client.BlazeClientBuilder
 import org.typelevel.log4cats.Logger
 import org.typelevel.log4cats.slf4j.Slf4jLogger
-
-import scala.concurrent.ExecutionContext
 
 object App extends IOApp {
 
@@ -18,16 +14,10 @@ object App extends IOApp {
 
   def stream[F[_]](args: List[String]): Stream[IO, Unit] =
     for {
-      botToken   <- Stream.eval(IO(BotToken(System.getenv("TELEGRAM_TOKEN"))))
+      token      <- Stream.eval(IO(BotToken(System.getenv("TELEGRAM_TOKEN"))))
       giphyToken <- Stream.eval(IO(GiphyToken(System.getenv("GIPHY_TOKEN"))))
-      client <- BlazeClientBuilder[IO](ExecutionContext.global)
-        .withCheckEndpointAuthentication(false)
-        .stream
-
-      api   = new TelegramBotApi(botToken, client)
-      giphy = new GiphyClient[IO](giphyToken)
-      bot   = new TelegramBot(api, giphy)
-    } yield bot.stream
+      _          <- new BotProcess[IO](token, giphyToken).stream
+    } yield ()
 
   override def run(args: List[String]): IO[ExitCode] =
     stream[IO](args).compile.drain.as(ExitCode.Success)
---
+++ src/main/scala/com/alisiikh/catharsis/bot/BotProcess.scala
@@ -0,0 +1,28 @@
+package com.alisiikh.catharsis.bot
+
+import cats.effect._
+import cats.implicits._
+import com.alisiikh.catharsis.bot.api.TelegramBotApi
+import com.alisiikh.catharsis.giphy.{ GiphyClient, GiphyToken }
+import fs2.Stream
+import org.typelevel.log4cats.Logger
+import org.http4s.blaze.client.BlazeClientBuilder
+
+import scala.concurrent.ExecutionContext
+
+class BotProcess[F[_]: Async: Logger](botToken: BotToken, giphyToken: GiphyToken) {
+
+  def stream: Stream[F, Unit] =
+    BlazeClientBuilder[F](ExecutionContext.global)
+      .withCheckEndpointAuthentication(false)
+      .stream
+      .flatMap { client =>
+        Stream.force {
+          for {
+            api   <- Sync[F].delay(new TelegramBotApi(botToken, client))
+            giphy <- Sync[F].delay(new GiphyClient(giphyToken))
+            bot   <- Sync[F].delay(new TelegramBot(api, giphy))
+          } yield bot.stream
+        }
+      }
+}
