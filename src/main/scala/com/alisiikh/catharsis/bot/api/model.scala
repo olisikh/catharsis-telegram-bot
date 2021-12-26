a35a8cedd33d3abb2d7bb5a38f8bc23efde8bed6
Oleksii Lisikh <alisiikh@gmail.com>
2021-12-26 20:02:13 +0100

    Deleted garbage

--- src/main/scala/com/alisiikh/catharsis/bot/api/model.scala
+++
@@ -1,9 +0,0 @@
-package com.alisiikh.catharsis.bot.api
-
-case class Chat(id: ChatId)
-
-case class BotMessage(message_id: Long, chat: Chat, text: Option[String])
-case class BotResponse[A](ok: Boolean, result: A)
-case class BotUpdate(update_id: Long, message: Option[BotMessage]) {
-  def chatId: Option[ChatId] = message.map(_.chat.id)
-}
